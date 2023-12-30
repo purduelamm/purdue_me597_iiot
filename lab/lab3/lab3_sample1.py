05d4a6f3ea818c74256b8684eaf30a3d73c30e13
FrankZhou1990 <zhouff1990@outlook.com>
2023-12-30 16:36:13 -0500

    content changed -fz

--- lab/lab3/lab3_sample1.py
+++ lab/lab3/lab3_sample1.py
@@ -1,4 +1,4 @@
-from pymodbus.client.sync import ModbusTcpClient
+from pymodbus.client import ModbusTcpClient
 from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadDecoder
 import datetime
@@ -16,14 +16,14 @@ now = datetime.datetime.now()
 
 # read the holding registers for frequency
 # Refer to the Modbus address map
-# the arguments are register starting register address (decimal), length, and unit=1.
-freq_read = client.read_holding_registers(1536, 2, unit=1)
+# the arguments are register starting register address (decimal), length, and slave=1.
+freq_read = client.read_holding_registers(1536, 2, slave=1)
 
 # get received registers
 freq_reg = freq_read.registers
 
 # define decode the received register values
-freq_decoder = BinaryPayloadDecoder.fromRegisters(freq_reg, byteorder=Endian.Big, wordorder=Endian.Big)
+freq_decoder = BinaryPayloadDecoder.fromRegisters(freq_reg, byteorder=Endian.BIG, wordorder=Endian.BIG)
 
 # get the frequency value by decoding it to 32bit float data.
 freq_value = freq_decoder.decode_32bit_float()
