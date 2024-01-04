797e6b2f2bcd46fe4b6dea42419d72c9658185d5
FrankZhou1990 <zhouff1990@outlook.com>
2024-01-03 21:34:33 -0500

    typo check

--- lab/lab5/xml_parsing_example/xml_parsing_example.py
+++ lab/lab5/xml_parsing_example/xml_parsing_example.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('xml_parsing_example.xml')
-root = tree.getroot()
+tree = ET.parse('xml_parsing_example.xml')  # create element tree object 
+root = tree.getroot()  # Get root element. root is the highest level of an xml tree which includes all the elements with lower levels.
 
-print(root[0].attrib['instanceId'])
+print(root[0].attrib['instanceId'])  # Header is the element with index 0 under root. root[0].attrib is a dictionary with keys of "creationTime", "sender", etc.
 
